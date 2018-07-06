6452a925225fef0888d72e7dbe2a76e9dd358c1a
Bryan Hughes <bryan@nebri.us>
2018-07-06 10:43:06 -0700

    Created the weekly scheduler algorithm

--- dist/main/scheduler.js
+++ dist/main/scheduler.js
@@ -17,6 +17,57 @@ along with Contact Schedular.  If not, see <http://www.gnu.org/licenses/>.
 */
 Object.defineProperty(exports, "__esModule", { value: true });
 var db_1 = require("./db");
+var moment = require("moment-timezone");
+var MAX_WEEKLY_CONTACTS = 10;
+var DAY_IN_MS = 1000 * 60 * 60 * 24;
+var MIN_MONTHLY_GAP = DAY_IN_MS * 25;
+var MONTHLY_GAP_SCALING_FACTOR = 0.1 / DAY_IN_MS;
+var MIN_QUARTERLY_GAP = DAY_IN_MS * 80;
+var QUARTERLY_GAP_SCALING_FACTOR = 0.05 / DAY_IN_MS;
+function refreshQueue(cb) {
+    var queue = db_1.dataSource.getQueue();
+    var lastUpdated = moment(queue.lastUpdated);
+    if (lastUpdated.isAfter(moment().startOf('week'))) {
+        setImmediate(cb);
+        return;
+    }
+    var contacts = db_1.dataSource.getContacts();
+    var weights = [];
+    for (var _i = 0, contacts_1 = contacts; _i < contacts_1.length; _i++) {
+        var contact = contacts_1[_i];
+        var gap = Date.now() - contact.lastContacted;
+        switch (contact.frequency) {
+            case 'weekly':
+                weights.push({
+                    contact: contact,
+                    weight: 1
+                });
+                break;
+            case 'monthly':
+                if (gap > MIN_MONTHLY_GAP) {
+                    weights.push({
+                        contact: contact,
+                        weight: Math.min(1, MONTHLY_GAP_SCALING_FACTOR * (gap - MIN_MONTHLY_GAP))
+                    });
+                }
+                break;
+            case 'quarterly':
+                if (gap > MIN_QUARTERLY_GAP) {
+                    weights.push({
+                        contact: contact,
+                        weight: Math.min(1, QUARTERLY_GAP_SCALING_FACTOR * (gap - MIN_QUARTERLY_GAP))
+                    });
+                }
+                break;
+        }
+    }
+    var newContactQueue = weights
+        .sort(function (a, b) { return b.weight - a.weight; })
+        .slice(0, MAX_WEEKLY_CONTACTS)
+        .map(function (weight) { return weight.contact; });
+    console.log("Scheduling " + newContactQueue.length + " contacts out of " + weights.length + " possible contacts");
+    db_1.setWeeklyQueue(newContactQueue, cb);
+}
 function init(cb) {
     db_1.dataSource.on('calendarsUpdated', function (calendars) {
         console.log(calendars);
@@ -27,7 +78,7 @@ function init(cb) {
     db_1.dataSource.on('queueUpdated', function (queue) {
         console.log(queue);
     });
-    setImmediate(cb);
+    refreshQueue(cb);
 }
 exports.init = init;
 //# sourceMappingURL=scheduler.js.map
--- main/src/scheduler.ts
+++ main/src/scheduler.ts
@@ -15,8 +15,60 @@ You should have received a copy of the GNU General Public License
 along with Contact Schedular.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { CB } from './common/types';
-import { dataSource } from './db';
+import { CB, IContact } from './common/types';
+import { dataSource, setWeeklyQueue } from './db';
+import * as moment from 'moment-timezone';
+
+const MAX_WEEKLY_CONTACTS = 10;
+const DAY_IN_MS = 1000 * 60 * 60 * 24;
+const MIN_MONTHLY_GAP = DAY_IN_MS * 25;
+const MONTHLY_GAP_SCALING_FACTOR = 0.1 / DAY_IN_MS;
+const MIN_QUARTERLY_GAP = DAY_IN_MS * 80;
+const QUARTERLY_GAP_SCALING_FACTOR = 0.05 / DAY_IN_MS;
+
+function refreshQueue(cb: CB): void {
+  const queue = dataSource.getQueue();
+  const lastUpdated = moment(queue.lastUpdated);
+  if (lastUpdated.isAfter(moment().startOf('week'))) {
+    setImmediate(cb);
+    return;
+  }
+  const contacts = dataSource.getContacts();
+  const weights: Array<{ contact: IContact; weight: number; }> = [];
+  for (const contact of contacts) {
+    const gap = Date.now() - contact.lastContacted;
+    switch (contact.frequency) {
+      case 'weekly':
+        weights.push({
+          contact,
+          weight: 1
+        });
+        break;
+      case 'monthly':
+        if (gap > MIN_MONTHLY_GAP) {
+          weights.push({
+            contact,
+            weight: Math.min(1, MONTHLY_GAP_SCALING_FACTOR * (gap - MIN_MONTHLY_GAP))
+          });
+        }
+        break;
+      case 'quarterly':
+        if (gap > MIN_QUARTERLY_GAP) {
+          weights.push({
+            contact,
+            weight: Math.min(1, QUARTERLY_GAP_SCALING_FACTOR * (gap - MIN_QUARTERLY_GAP))
+          });
+        }
+        break;
+    }
+  }
+  const newContactQueue: IContact[] = weights
+    .sort((a, b) => b.weight - a.weight)
+    .slice(0, MAX_WEEKLY_CONTACTS)
+    .map((weight) => weight.contact);
+  console.log(`Scheduling ${newContactQueue.length} contacts out of ${weights.length} possible contacts`);
+  setWeeklyQueue(newContactQueue, cb);
+}
 
 export function init(cb: CB): void {
   dataSource.on('calendarsUpdated', (calendars) => {
@@ -28,5 +80,5 @@ export function init(cb: CB): void {
   dataSource.on('queueUpdated', (queue) => {
     console.log(queue);
   });
-  setImmediate(cb);
+  refreshQueue(cb);
 }
